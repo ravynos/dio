62f6434ef5273fcdc5105cee5c98d382ea85532f
Ravynos <th.sobrinho@gmail.com>
2022-11-22 17:18:34 -0300

    iniciando functions

---
+++ BootCamps/OrangeTech/JS/04 - Funcoes/aula.js
@@ -0,0 +1,19 @@
+/*function teste() {
+    console.log('teste')
+}
+
+teste();*/
+
+/*function digaSeuNome(nome) {
+    console.log('Seu nome é '+ nome)
+}
+
+digaSeuNome('Thiago');
+digaSeuNome('João')*/
+
+function quadrado(valor) {
+ return valor * valor
+}
+
+const quadradoDeDez = quadrado(10)
+console.log(quadradoDeDez)
