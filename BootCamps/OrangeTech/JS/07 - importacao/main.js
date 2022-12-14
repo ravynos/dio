a6ceadcc432537dd7876e7702b45d6881394d273
Ravynos <th.sobrinho@gmail.com>
2022-12-14 17:41:38 -0300

    importação

---
+++ BootCamps/OrangeTech/JS/07 - importacao/main.js
@@ -0,0 +1,24 @@
+/*
+
+    Uma sala comtem 5 alunos e para cada aluno foi sorteado um número de 1 - 100.
+    Faça um programa que receba os 5 numeros sorteados para os  alunos e mostre o maior 
+    número sorteado.
+
+    Dados de entrada:
+    5
+    50
+    10
+    98
+    23
+
+    Saída:
+    98
+*/
+
+const { gets, print } = require('./funcoes-auxiliares')
+
+print(gets())
+print(gets())
+print(gets())
+print(gets())
+print(gets())
